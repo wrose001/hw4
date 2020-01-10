c81d0d937bddd16e226ff94474b944c556ce6a6d
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-01-10 11:04:07 -0600

    Edited my html document, removing the card header from the bootstrap that I injected into my code so that my card more closely resembles the homework example. After texting code with the inspector tool in Google Chrome, added code in CSS file to change the color of the button, center the card text. Also added a bit of JS as I begin to think through the JS logic.

---
+++ script.js
@@ -0,0 +1,8 @@
+//Traverse through titles
+for(var count=0; count < Array.length; count++){
+    var ul=document.createElement("ul");
+    
+    main.append();
+}
+
+//Need another for loop to traverse through choices, needs to be nested
