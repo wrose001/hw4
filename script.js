ae3413027f21c4362e244ebf803ae435f1d30e66
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-01-12 12:15:24 -0600

    Created a for loop to run through the questions array, dynamically adding each question for the quiz in the JS file. Also, commented out a section of code in the html file, leaving an empty div to push the questions into from JS.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var questionsElem = document.querySelector("#questions");
+
 var questions = [
     {
         title: "Commonly used data types DO NOT include:",
@@ -27,10 +29,45 @@ var questions = [
 ];
 
 //Traverse through titles
-for(var count=0; count < Array.length; count++){
-    var ul=document.createElement("ul");
+// for(var count=0; count < Array.length; count++){
+//     var ul=document.createElement("ul");
     
-    main.append();
+//     main.append();
+// }
+{/* <div class="card container">
+                    <div class="card-body">
+                      <h5 class="card-title"></h5>
+                      <p class="card-text"></p>
+                      <ul><li id="startBtn" class="btn btn-primary"></li>
+                        <li id="startBtn" class="btn btn-primary"></li>
+                        <li id="startBtn" class="btn btn-primary"></li>
+                        <li id="startBtn" class="btn btn-primary"></li>
+                      </ul>
+                    </div>
+                  </div> */}
+for(var count=0; count < questions.length; count++){
+    var card = document.createElement("div");
+    card.setAttribute("class", "card container");
+
+    var cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body");
+
+    var h5 = document.createElement("h5");
+    h5.setAttribute("class", "card-title");
+    var title = questions[count].title;
+    h5.textContent = title;
+    cardBody.append(h5);
+
+
+
+
+
+    card.append(cardBody);
+    questionsElem.append(card);
+
+
+    // var ul=document.createElement("ul");
+    // 
 }
 
 //Need another for loop to traverse through choices, needs to be nested
