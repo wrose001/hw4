89c174e123fc152db18396b524fd10cd9f8e0c2a
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-01-13 20:20:49 -0600

    Updated script.js code with code that Calvin helped me with to make questions in question.js append/hide as a user clicks a choice. Still need to write code to add data to localStorage and create timer.

--- script.js
+++ script.js
@@ -1,112 +1,126 @@
-var main = document.querySelector("main");
+var startSection = document.querySelector("#start"); // Start section that will show 1st, along with start button
+var startBtn = document.querySelector("#startBtn"); // button to start the quiz
+
+var questionSection = document.querySelector("#main"); // We use this selector to append all of the card layout questions, querySelector requires an id (#), or class (.), or tag (i.e. div). In our case we are grabbing the div by its id <div id="main">
+
+var completedSection = document.querySelector("#completed"); // Once quiz is complete we should show the completed section
 
 var currentQuestion = 0;
 
 var renderQuestions = function() {
 
-    //main.innerHTML = " ";
+    questionSection.innerHTML = " ";
 
-    myClass.forEach(function (question, indexQuestion){
-        var div = document.createElement("div");
+    questions.forEach(function (question, indexQuestion) {
 
-        //If item isn't current question, hide it
-        if(currentQuestion != indexQuestion){
-            div.setAttribute("class", "hide");
-        }
+        /*
+            The following code within this function will build cards under the main div like such:
 
-        //Create h1
-        var h5 = document.createElement("h1");
-        h5.textContent = question.title;
+            <div class="card container">
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <div class="choices">
+                        <button class="btn btn-primary center-btn">choice1</button>
+                        <button class="btn btn-primary center-btn">choice2</button>
+                        <button class="btn btn-primary center-btn">choice3</button>
+                        <button class="btn btn-primary center-btn">choice4</button>
+                    </div>
+                </div>
+            </div>
+        */
+        var card = document.createElement("div");
+        card.setAttribute("class", "card container");
 
-        //Create ul
-        var ul = document.createElement("ul");
+        var cardBody = document.createElement("div");
+        cardBody.setAttribute("class", "card-body");
 
-        //Create li tags
+        var h5 = document.createElement("h5");
+        h5.setAttribute("class", "card-title");
+        h5.textContent = question.title; // add question title to h5
+        cardBody.append(h5); // append h5 to cardBody
+
+        // Create choicesSection div
+        var choicesSection = document.createElement("div");
+        choicesSection.setAttribute("class", "choices");
+
+        // Create button choice tags
         question.choices.forEach(function(answerChoice){
-            var li = document.createElement("li");
-            li.textContent = answerChoice;
+            var button = document.createElement("button");
+            button.textContent = answerChoice;
+            button.setAttribute("class", "btn btn-primary center-btn");
 
-            //Append li to ul tag
-            ul.append(li);
+            /*
+                Weston right here it may be best to add the answer to the button.
+
+                Here's an option you can do:
+
+                You could CREATE a data attribute
+                Refer to activity:
+                https://github.com/the-Coding-Boot-Camp-at-UT/UT-AUS-FSF-PT-12-2019-U-C/tree/master/04-Web-APIs/01-Activities/24-Ins_Data-Attributes
+
+                i.e. correct choice is 
+                <button data-answer="true">choice1</button>
+
+                incorrect choice is
+                <button data-answer="false">choice2</button>
+                <button data-answer="false">choice3</button>
+                <button data-answer="false">choice4</button>
+
+                Later in the questionSection.addEventListener section
+                You can read what the user clicked on. If the user clicked
+                on a button that has the data-answer attribute set to "true"
+                that means question was correct, if "false" then its wrong
+
+            */
+
+            //Append button to div choices tag
+            choicesSection.append(button);
+
         });
 
-            //Append h5 to div
-            div.append(h5);
+        // hide card if its not the currentQuestion the user should see
+        if(currentQuestion != indexQuestion) {
+            card.setAttribute("class", "hide");
+        }
 
-            //Append ul to div
-            div.append(ul);
-
-            //Append div (which now has the h5 and ul) to main element
-            main.append(div);
+        cardBody.append(choicesSection); // append choicesSection to cardBody
+        card.append(cardBody); // append cardBody to card
+        questionSection.append(card); // finally append card to main div
     });
 
 }
 
-//Initial - ender questions
-renderQuestions();
+// Use delegator strategy, have event listener on predefined element main
+// Wait for clicks on main element
+questionSection.addEventListener("click", function(event){
+    var targetedClick = event.target;
 
-    //Use delegator strategy, have event listener on predefined element main
-    //Wait for clicks on main element
-    main.addEventListener("click", function(event){
+    //Only target li tag to proceed to next classroom
+    if(targetedClick.matches("button")) {
+        console.log(targetedClick.textContent);
+        currentQuestion++;
 
-        var targetedClick = event.target;
+        // currentQuestion that user see must be less than the size of our questions array.
+        if(currentQuestion < questions.length) {
+         renderQuestions();
+        } else {
 
-        //Only target li tag to proceed to next classroom
-        if(targetedClick.matches("li")){
+            /*
+            This means the user has clicked through all the questions (completed all questions). We should now show the next div which is the "completed" section that's in the index.html
+            */
 
-            console.log(targetedClick.textContent);
-            currentQuestion++;
-            renderQuestions();
+            questionSection.setAttribute("class", 'hide');
+            completedSection.setAttribute("class", "show");
 
         }
+    }
+});
 
-    });
+// Start the Quiz when user clicks on a start button
+startBtn.addEventListener("click", function(event) {
+    questionSection.setAttribute("class", "show");
+    startSection.setAttribute("class", "hide");
 
-
-
-//Traverse through titles
-// for(var count=0; count < Array.length; count++){
-//     var ul=document.createElement("ul");
-    
-//     main.append();
-// }
-{/* <div class="card container">
-                    <div class="card-body">
-                      <h5 class="card-title"></h5>
-                      <p class="card-text"></p>
-                      <ul><li id="startBtn" class="btn btn-primary"></li>
-                        <li id="startBtn" class="btn btn-primary"></li>
-                        <li id="startBtn" class="btn btn-primary"></li>
-                        <li id="startBtn" class="btn btn-primary"></li>
-                      </ul>
-                    </div>
-                  </div> */}
-// for(var count=0; count < questions.length; count++){
-//     var card = document.createElement("div");
-//     card.setAttribute("class", "card container");
-
-//     var cardBody = document.createElement("div");
-//     cardBody.setAttribute("class", "card-body");
-
-//     var h5 = document.createElement("h5");
-//     h5.setAttribute("class", "card-title");
-//     var title = questions[count].title;
-//     h5.textContent = title;
-//     cardBody.append(h5);
-
-//     var unorderedList = document.createElement("ul");
-//     unorderedList.setAttribute("class", "ul");
-//     var answerChoices = questions[count].choices;
-//     unorderedList.textContent = answerChoices;
-//     h5.append(unorderedList);
-
-
-//     card.append(cardBody);
-//     questionsElem.append(card);
-
-
-    // var ul=document.createElement("ul");
-    // 
-// }
-
-//Need another for loop to traverse through choices, needs to be nested
+    // Render Questions
+    renderQuestions();
+});
